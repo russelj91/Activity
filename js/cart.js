04e4e8dab5b5250027b405a67c6d2c529b2414e8
russelj91 <russelj91@gmail.com>
2023-01-30 01:17:55 +0800

    new file:   New folder/expres.png 	new file:   New folder/kotlin.png 	new file:   New folder/magic.png 	deleted:    clone.html 	modified:   css/main.css 	modified:   index.html 	new file:   js/cart.js 	new file:   js/clone.js 	modified:   pages/css/style.css 	modified:   pages/webdeveloper.html

---
+++ js/cart.js
@@ -0,0 +1,44 @@
+let addItemId = 0;
+
+
+    function addToCart(item) {
+      addItemId += 1;
+
+      let selectedItem = document.createElement('div');
+      selectedItem.classList.add('cartImg');
+      selectedItem.setAttribute('id', addItemId);
+
+      let img = document.createElement('img');
+      img.setAttribute('src', item.children[0].currentSrc);
+
+      let title = document.createElement('div');
+      title.innerText = item.children[1].innerText;
+
+      let label = document.createElement('div');
+      label.innerText = item.children[0].innerText;
+      let delBtn = document.createElement('button');
+
+      delBtn.innerText = 'Delete';
+      delBtn.setAttribute('onclick','del('+addItemId+')');
+
+      let cartItems = document.getElementById('title');
+      document.getElementById("title").style.fontStyle = "italic";
+      
+      selectedItem.append(img);
+      selectedItem.append(title);
+      selectedItem.append(label);
+      selectedItem.append(delBtn);
+      cartItems.append(selectedItem);
+
+    }
+
+    function del(item){
+        document.getElementById(item).remove();
+     
+    }
+
+
+    function noTice() {
+      alert("Before proceeding, remember that refreshing the page will remove the Save Courses! Remember the courses before deleting!");
+    }
+
